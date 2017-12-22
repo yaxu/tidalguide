9006def6178fbd6d2a74a7c2d7da3f9dc8dd95ec
alex <alex@slab.org>
2017-12-22 21:50:05 +0000

    + ‘compile.sh’

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -d "$HOME/.cabal/bin" ] ; then
+    PATH="$HOME/.cabal/bin:$PATH"
+fi
+
+~/.cabal/bin/pandoc -s 0*.md --pdf-engine=xelatex --variable mainfont="Brill" --bibliography all.bib --filter render.hs -o pihkal.pdf
