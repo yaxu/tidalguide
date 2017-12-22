72d981c1eec624f8673dfe5a65b3590136586ee4
alex <alex@slab.org>
2017-12-22 21:52:09 +0000

    - ‘.#compile.sh’ / ‘compile.sh’

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ if [ -d "$HOME/.cabal/bin" ] ; then
     PATH="$HOME/.cabal/bin:$PATH"
 fi
 
-~/.cabal/bin/pandoc -s 0*.md --pdf-engine=xelatex --variable mainfont="Brill" --bibliography all.bib --filter render.hs -o all.pdf
-~/.cabal/bin/pandoc -s 0*.md --pdf-engine=xelatex --variable mainfont="Brill" --bibliography all.bib --filter render.hs -o all.html
+pandoc -s 0*.md --pdf-engine=xelatex --variable mainfont="Brill" --bibliography all.bib --filter render.hs -o all.pdf
+pandoc -s 0*.md --pdf-engine=xelatex --variable mainfont="Brill" --bibliography all.bib --filter render.hs -o all.html
